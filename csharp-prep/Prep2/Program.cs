1dc70017e0cd66ae95be5ddf3a7b6fefc8f9c4d5
macowen16 <Cra20019@byui.edu>
2025-04-28 22:01:59 -0600

    prep_2_first

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
-}
+
+         Console.WriteLine("What was your score?");
+        string valueFromUser = Console.ReadLine();
+
+        int score;
+        if (int.TryParse(valueFromUser, out score))
+        {
+            if (score >= 90)
+            {
+                Console.WriteLine("You got an A!");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("You got a B!");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("You got a C!");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("You got a D!");
+            }
+            else
+            {
+                Console.WriteLine("You got an F.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+        }
+    }}
