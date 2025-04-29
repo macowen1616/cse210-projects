a9e1df89f5378ceeee313b31df681ec4b10764fe
macowen16 <Cra20019@byui.edu>
2025-04-28 20:58:23 -0600

    Activity#1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your  Firstname?");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your  Lastname?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"My name is {last_name}, {first_name} {last_name}");
+
+
     }
 }
