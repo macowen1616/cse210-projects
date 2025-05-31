8dbc4ca481e6506884ba1e6d740034bbdfd8b027
macowen16 <Cra20019@byui.edu>
2025-05-30 18:21:30 -0600

    update#1

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _rand = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        var visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        if (visibleWords.Count == 0) return;
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = _rand.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} {text}";
+    }
+
+    public bool AllWordsHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
