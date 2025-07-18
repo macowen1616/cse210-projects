3920a3e547fa5d241057c01ea3f350e81ca21048
macowen16 <Cra20019@byui.edu>
2025-07-17 20:18:26 -0600

    update

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,16 +7,16 @@ class Program
     {
         List<Video> videos = new List<Video>();
 
-        Video video1 = new Video("How to Cook Pasta", "Chef Tony", 300);
+        Video video1 = new Video("How to Cook Pasta", "Chef Makay", 300);
         video1.AddComment(new Comment("Alice", "This was so helpful, thanks!"));
         video1.AddComment(new Comment("Bob", "Now I'm hungry lol."));
         video1.AddComment(new Comment("Charlie", "Tried this today and loved it!"));
         videos.Add(video1);
 
-        Video video2 = new Video("Top 10 Hiking Trails", "Wilderness Gal", 540);
-        video2.AddComment(new Comment("Dave", "Can’t wait to go hiking now."));
-        video2.AddComment(new Comment("Eve", "Number 4 is my favorite trail!"));
-        video2.AddComment(new Comment("Frank", "Awesome recommendations."));
+        Video video2 = new Video("Extreme Airsoft", "Sierra Nevada Rangers", 540);
+        video2.AddComment(new Comment("Dave", "Can’t wait to go Airsofting now."));
+        video2.AddComment(new Comment("Eve", "Those BBs were flying like crazy!!"));
+        video2.AddComment(new Comment("Frank", "Awesome!"));
         videos.Add(video2);
 
         Video video3 = new Video("Learn C# in 10 Minutes", "CodeMaster", 600);
@@ -25,10 +25,10 @@ class Program
         video3.AddComment(new Comment("Isaac", "Wish I saw this earlier."));
         videos.Add(video3);
 
-        Video video4 = new Video("Funny Cat Compilation", "Animal Planet", 180);
-        video4.AddComment(new Comment("Jake", "I laughed so hard!"));
-        video4.AddComment(new Comment("Lily", "Too cute!"));
-        video4.AddComment(new Comment("Mia", "Made my day better."));
+        Video video4 = new Video("Top 10 most Dangerous Animals", "Animal Planet", 180);
+        video4.AddComment(new Comment("Jake", "Soooo Dangerous!"));
+        video4.AddComment(new Comment("Lily", "Some are really cute!"));
+        video4.AddComment(new Comment("Mia", "We have to watch out!"));
         videos.Add(video4);
 
         foreach (Video vid in videos)
