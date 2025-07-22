d259863546b50ab97792dbc75ab61d98b8fec732
macowen16 <Cra20019@byui.edu>
2025-07-21 18:38:07 -0600

    update

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         int choice = 0;
 
-        while (choice != 6)
+        while (choice != 4) 
         {
             Console.WriteLine("\nMenu Options:");
             Console.WriteLine("1. Running");
@@ -14,30 +14,40 @@ class Program
             Console.WriteLine("3. Swimming");
             Console.WriteLine("4. Quit");
             Console.Write("Select a choice from the menu: ");
-            choice = int.Parse(Console.ReadLine());
-if (choice == 1)
-{
-    RunningAct Ract = new RunningAct();
-    Ract.PerformActivity();
-}
-else if (choice == 2)
-{
-BikingAct Bact = new BikingAct();
-    Bact.PerformActivity(); 
-}
-else if (choice == 3)
-{
-    SwimmingAct Sact = new SwimmingAct();
-    Sact.PerformActivity();
-}
 
+            
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a number from 1 to 4.");
+                continue;
+            }
+
+            if (choice == 1)
+            {
+                RunningAct ract = new RunningAct();
+                ract.PerformActivity();
+                Console.WriteLine(); // Optional spacing
+            }
+            else if (choice == 2)
+            {
+                BikingAct bact = new BikingAct();
+                bact.PerformActivity();
+                Console.WriteLine();
+            }
+            else if (choice == 3)
+            {
+                SwimmingAct sact = new SwimmingAct();
+                sact.PerformActivity();
+                Console.WriteLine();
+            }
             else if (choice == 4)
             {
                 Console.WriteLine("Goodbye!");
             }
             else
             {
-                Console.WriteLine("Invalid choice. Please select 1-4.");
+                Console.WriteLine("Invalid choice. Please select a number from 1 to 4.");
             }
         }
     }
